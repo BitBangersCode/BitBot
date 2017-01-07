79f5da3b1a31d2d870ea0ab2c1efc80320c90349
Josh <josh.rickers@googlemail.com>
2017-01-07 12:47:24 +0000

    Update
    Indents, command variable.

--- bot.js
+++ bot.js
@@ -7,29 +7,35 @@ const bot = new Discord.Client();
 let ballAnswers = JSON.parse(fs.readFileSync('./8ball.json', 'utf8'));
 
 bot.on('ready', () => {
-        console.log('BitBot is ready');
+  console.log('BitBot is ready');
 });
 
 bot.on('message', msg => {
-        if (!msg.content.startsWith(config.prefix)) return;
-        if (msg.author.bot) return;
-        if (msg.content.startsWith(config.prefix + 'ping')) {
-                msg.channel.sendMessage('pong!');
-        }
-        if (msg.content.startsWith(config.prefix + 'help')) {
-                msg.channel.sendMessage('I am a noob and can only reply to !ping :(');
-        }
-        if (msg.content.startsWith(config.prefix + '8ball')) {
-                msg.channel.sendMessage(ballAnswers[Math.floor(Math.random() * 20) +1]);
-        }
-        if (msg.content.startsWith(config.prefix + 'russian')) {
-                let bullet = 3;
-                if (Math.floor(Math.random() * 6) + 1 == bullet) {
-                        msg.channel.sendMessage('BANG!');
-                } else {
-                        msg.channel.sendMessage('CLICK');
-                }
-        }
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(config.prefix)) return;
+
+  let command = message.content.split(' ')[0];
+  command = command.slice(config.prefix.length);
+
+  if (command == 'ping')) {
+    msg.channel.sendMessage('pong!');
+  }
+
+  if (command == 'help')) {
+    msg.channel.sendMessage('I am a noob and can only reply to !ping :(');
+  }
+
+  if (command == '8ball')) {
+    msg.channel.sendMessage(ballAnswers[Math.floor(Math.random() * 20) +1]);
+  }
+  if (msg.content.startsWith(config.prefix + 'russian')) {
+    let bullet = 3;
+    if (Math.floor(Math.random() * 6) + 1 == bullet) {
+      msg.channel.sendMessage('BANG!');
+    } else {
+      msg.channel.sendMessage('CLICK');
+    }
+  }
 });
 
 bot.login(config.token);
