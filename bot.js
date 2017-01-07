5ab11c6e50a409a2745748f95a2edbb30dca401c
Josh <josh.rickers@googlemail.com>
2017-01-07 12:28:42 +0000

    Bot

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const config = require("./config.json");
+const fs = require("fs");
+
+const bot = new Discord.Client();
+
+let ballAnswers = JSON.parse(fs.readFileSync('./8ball.json', 'utf8'));
+
+bot.on('ready', () => {
+        console.log('BitBot is ready');
+});
+
+bot.on('message', msg => {
+        if (!msg.content.startsWith(config.prefix)) return;
+        if (msg.author.bot) return;
+        if (msg.content.startsWith(config.prefix + 'ping')) {
+                msg.channel.sendMessage('pong!');
+        }
+        if (msg.content.startsWith(config.prefix + 'help')) {
+                msg.channel.sendMessage('I am a noob and can only reply to !ping :(');
+        }
+        if (msg.content.startsWith(config.prefix + '8ball')) {
+                msg.channel.sendMessage(ballAnswers[Math.floor(Math.random() * 20) +1]);
+        }
+        if (msg.content.startsWith(config.prefix + 'russian')) {
+                let bullet = 3;
+                if (Math.floor(Math.random() * 6) + 1 == bullet) {
+                        msg.channel.sendMessage('BANG!');
+                } else {
+                        msg.channel.sendMessage('CLICK');
+                }
+        }
+});
+
+bot.login(config.token);
