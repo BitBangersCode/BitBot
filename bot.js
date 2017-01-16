a8953807c3af1f49d1fa3ba775761f2b510cb81a
Josh <josh.rickers@googlemail.com>
2017-01-16 16:50:10 +0000

    kill command / indenting fucked

--- bot.js
+++ bot.js
@@ -37,6 +37,24 @@ bot.on('message', message => {
 
   let args = message.content.split(' ').slice(1);
 
+  if (command == 'kill'){
+    let role = message.guild.roles.find('name', config.deathRole);
+    if (!args[0]) {
+      if (message.member.roles.has(role.id)) {
+        message.channel.sendMessage(`${message.member} is already dead.`);
+      } else {
+        message.member.addRole(role);
+        message.channel.sendMessage(`${message.member} has been killed and sent to hell!`);
+      }
+    } else {
+      let mention = message.mentions.users.first();
+      if (mention && !mention.bot) {
+      } else {
+        message.channel.sendMessage(`Cannot kill user. Either a group mention, a bot, or user does not exist`);
+      }
+    }
+  }
+
   if (command == 'say') {
     message.channel.sendMessage(args.join(' '));
   }
@@ -61,83 +79,82 @@ Stats for ${message.member.user.username}
         message.channel.sendMessage(`No stats for user. Either a group mention, a bot, or user does not exist`);
       }
     }
+  }
 
-    }
+  if (command == 'ping') {
+    message.channel.sendMessage('pong!');
+  }
 
-    if (command == 'ping') {
-      message.channel.sendMessage('pong!');
-    }
+  if (command == 'help') {
+    message.channel.sendMessage(helpMessage);
+  }
 
-    if (command == 'help') {
-      message.channel.sendMessage(helpMessage);
-    }
+  if (command == '8ball') {
+    message.channel.sendMessage(ballAnswers[Math.floor(Math.random() * 20) +1]);
+  }
 
-    if (command == '8ball') {
-      message.channel.sendMessage(ballAnswers[Math.floor(Math.random() * 20) +1]);
-    }
-
-    if (command == 'russian') {
-      let chamber = 1;
-      if (!args[0] || args[0] == 'help') {
+  if (command == 'russian') {
+    let chamber = 1;
+    if (!args[0] || args[0] == 'help') {
         message.channel.sendMessage(`\`\`\`
 Usage:
           ${config.prefix}russian  - Russian Roulette. Death = 10 minute mute!
                     - load  - Loads a bullet.
                     - spin  - Spins the chamber.
                     - pull  - Pulls the trigger.
-          \`\`\``);
-        }
-        if (args[0] == 'load') {
-          if (bullet == 0) {
-            bullet = Math.floor(Math.random() * 6) + 1;
-            message.channel.sendMessage('Loading bullet...');
-          } else if (bullet > 0) {
-            message.channel.sendMessage('Bullet already loaded...');
-          }
-        }
-        if (args[0] == 'pull') {
-          if (bullet > 0) {
-            if (bullet == chamber) {
-              message.channel.sendMessage('BANG!');
-              stats[message.author.id].deaths++;
-              let role = message.guild.roles.find('name', config.deathRole);
-              if (role) {
-                if (message.member.roles.has(role.id)) {
-                  message.channel.sendMessage(`${message.member} is already dead but somehow manages to shoot themselves anyway. Must be a zombie?`)
-                } else {
-                  message.member.addRole(role).catch(console.error);
-                  message.channel.sendMessage(`${message.member}s brains explode! Rest in peace.`);
-                  setTimeout(function() {
-                    message.channel.sendMessage(`${message.member} has risen from the dead!`);
-                    message.member.removeRole(role).catch(console.error);
-                  }, 600000);
-                }
-              } else {
-                message.channel.sendMessage('No Death role. Please add Death role and update config.');
-              }
-              bullet -= 1;
+        \`\`\``);
+    }
+    if (args[0] == 'load') {
+      if (bullet == 0) {
+        bullet = Math.floor(Math.random() * 6) + 1;
+        message.channel.sendMessage('Loading bullet...');
+      } else if (bullet > 0) {
+        message.channel.sendMessage('Bullet already loaded...');
+      }
+    }
+    if (args[0] == 'pull') {
+      if (bullet > 0) {
+        if (bullet == chamber) {
+          message.channel.sendMessage('BANG!');
+          stats[message.author.id].deaths++;
+          let role = message.guild.roles.find('name', config.deathRole);
+          if (role) {
+            if (message.member.roles.has(role.id)) {
+              message.channel.sendMessage(`${message.member} is already dead but somehow manages to shoot themselves anyway. Must be a zombie?`)
             } else {
-              message.channel.sendMessage('CLICK!');
-              bullet -= 1;
+              message.member.addRole(role).catch(console.error);
+              message.channel.sendMessage(`${message.member}s brains explode! Rest in peace.`);
+              setTimeout(function() {
+                message.channel.sendMessage(`${message.member} has risen from the dead!`);
+                message.member.removeRole(role).catch(console.error);
+              }, 600000);
             }
           } else {
-            message.channel.sendMessage("Please load the gun...");
+            message.channel.sendMessage('No Death role. Please add Death role and update config.');
           }
+          bullet -= 1;
+        } else {
+          message.channel.sendMessage('CLICK!');
+          bullet -= 1;
         }
-        if (args[0] == 'spin') {
-          if (bullet > 0){
-            bullet = Math.floor(Math.random() * 6) + 1;
-            message.channel.sendMessage('Spinning chamber...');
-          } else {
-            message.channel.sendMessage("Please load the gun...");
-          }
-        }
+      } else {
+        message.channel.sendMessage("Please load the gun...");
       }
-      fs.writeFile('./stats.json', JSON.stringify(stats), (err) => {
-        if (err) {
-          console.log(err)
-        }
-      });
-    });
+    }
+    if (args[0] == 'spin') {
+      if (bullet > 0){
+        bullet = Math.floor(Math.random() * 6) + 1;
+        message.channel.sendMessage('Spinning chamber...');
+      } else {
+        message.channel.sendMessage("Please load the gun...");
+      }
+    }
+  }
+  fs.writeFile('./stats.json', JSON.stringify(stats), (err) => {
+    if (err) {
+      console.log(err)
+    }
+  });
+});
 
-    bot.login(config.token);
+  bot.login(config.token);
