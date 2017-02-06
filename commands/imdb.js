ee02e670a2622d1b369efa913c1dc32366a1a05a
Josh <josh.rickers@googlemail.com>
2017-02-06 17:02:11 +0000

    purge command, updated IMDB

--- commands/imdb.js
+++ commands/imdb.js
@@ -1,19 +1,40 @@
+const Discord = require('discord.js');
 var request = require('request');
+const green = '#008000';
+const red = '#FF0000';
+const yellow = '#FFD700';
+
 exports.run = function(client, message, args){
+  var embed = new Discord.RichEmbed();
   let url = `http://www.omdbapi.com/?t=${args[0]}&y=&plot=short&r=json`;
   request(url, function (error, response, filmData) {
     if (error) {
       return console.log(error);
     }
     if (response.statusCode != 200) {
-      return console.log(response.statusCode);
+      message.channel.sendMessage(`There has been an error. Response code: ${response.statusCode}`)
     }
-    console.log(filmData);
+    if (filmData == 'The service is unavailable.') {
+      return message.channel.sendMessage('The service is unavailable.');
+    }
+    if (filmData.)
+    filmData = JSON.parse(filmData);
+    //embed.setTitle(filmData.Title);
+    embed.setDescription
+    embed.setColor(red);
+    //embed.setURL(`http://www.imdb.com/title/${filmData.imdbID}/`);
+    embed.setThumbnail(filmData.Poster);
+    embed.addField('Genre', filmData.Genre);
+    embed.addField('Rating', `${filmData.Metascore}   - Meta Score
+${filmData.imdbRating}  - IMDB Rating`);
+    embed.addField('Plot', filmData.Plot);
+
+    message.channel.sendEmbed(embed);
   });
 };
 
 exports.conf = {
-  enabled: false,
+  enabled: true,
   guildOnly: false,
   aliases: [],
   permLevel: 0
