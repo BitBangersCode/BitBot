3e296d9bee4f6edd4c2358ee8c1ef5dee6c8f6a9
Josh <josh.rickers@googlemail.com>
2017-02-13 15:42:04 +0000

    functions and callbacks

--- commands/movie.js
+++ commands/movie.js
@@ -9,11 +9,65 @@ exports.run = function(client, message, args) {
   if (!args[0]){
       return message.channel.sendMessage('Please enter a search term.');
   }
-
   args = args.join(' ');
   var embed = new Discord.RichEmbed();
   var colour;
-  var filmID;
+  getSearchData(args, function(searchData) {
+    if (searchData.total_results == 0) {
+      message.channel.sendMessage('Movie not found');
+    } else {
+      getFilmData(searchData.results[0].id, function(filmData) {
+        if (parseFloat(filmData.vote_average)*10 >= 80){
+          colour = green;
+        } else if (parseFloat(filmData.vote_average)*10 >= 60 && parseFloat(filmData.vote_average)*10 < 80) {
+          colour = yellow;
+        }else if (parseFloat(filmData.vote_average)*10 < 60){
+          colour = red;
+        }
+
+        embed.setDescription(`__**${filmData.title}**__`);
+        embed.setColor(colour);
+        embed.setThumbnail(`https://image.tmdb.org/t/p/w500${filmData.poster_path}`);
+        embed.addField('Release Date', `${filmData.release_date}`);
+        embed.addField('Runtime', `${filmData.runtime} minutes`);
+        embed.addField('Rating', `${filmData.vote_average}  -  TheMovieDB User Score`);
+        embed.addField('Plot', filmData.overview);
+
+        message.channel.sendEmbed(embed);
+      });
+    }
+  });
+};
+
+function getFilmData(filmID, callback) {
+  var filmOptions = {
+    method: 'GET',
+    url: `https://api.themoviedb.org/3/movie/${filmID}`,
+    qs: {
+      language: 'en-US',
+      api_key: config.tmdbKey
+    },
+    body: '{}'
+  };
+
+  request(filmOptions, function (error, response, filmData) {
+    if (error || response.statusCode != 200) {
+      console.log(error);
+      message.channel.sendMessage('There has been an error searching, please try again later.');
+      return;
+    }
+    try{
+      filmData = JSON.parse(filmData);
+    } catch(e) {
+      console.log(e);
+      message.channel.sendMessage('There has been an error searching, please try again later.');
+      return;
+    }
+    return callback(filmData);
+  });
+}
+
+function getSearchData(args, callback) {
   var searchOptions = {
     method: 'GET',
     url: 'https://api.themoviedb.org/3/search/movie',
@@ -28,79 +82,21 @@ exports.run = function(client, message, args) {
   };
 
   request(searchOptions, function (error, response, searchData) {
-    if (error) {
+    if (error || response.statusCode != 200) {
       console.log(error);
       message.channel.sendMessage('There has been an error searching, please try again later.');
       return ;
     }
-    if (response.statusCode != 200) {
-      console.log(response.statusCode);
-      return message.channel.sendMessage('There has been an error searching, please try again later.');
-    }
     try{
       searchData = JSON.parse(searchData);
-    } catch(e) {
-      console.log(e);
-      message.channel.sendMessage('There has been an error searching, please try again later.');
-      return;
-    }
-    if (searchData.total_results == 0) {
-      message.channel.sendMessage('No movie found');
-    } else {
-      return filmID = searchData.results[0].id;
-    }
-  });
-
-console.log(filmID);
-  var filmOptions = {
-    method: 'GET',
-    url: `https://api.themoviedb.org/3/movie/${filmID}`,
-    qs: {
-      language: 'en-US',
-      api_key: config.tmdbKey
-    },
-    body: '{}'
-  };
-
-  console.log(filmOptions);
-  request(filmOptions, function (error, response, filmData) {
-    if (error) {
+    } catch(error) {
       console.log(error);
       message.channel.sendMessage('There has been an error searching, please try again later.');
       return;
     }
-    if (response.statusCode != 200) {
-      console.log(response.statusCode);
-      return message.channel.sendMessage('There has been an error searching, please try again later.');
-    }
-    try{
-      filmData = JSON.parse(filmData);
-    } catch(e) {
-      console.log(e);
-      message.channel.sendMessage('There has been an error searching, please try again later.');
-      return;
-    }
-    if (parseFloat(filmData.vote_average)*10 >= 80){
-      colour = green;
-    }
-    if (parseFloat(filmData.vote_average)*10 >= 60 && parseFloat(filmData.vote_average)*10 < 80) {
-      colour = yellow;
-    }
-    if (parseFloat(filmData.vote_average)*10 < 60){
-      colour = red;
-    }
-
-    embed.setDescription(`__**${filmData.title}**__`);
-    embed.setColor(colour);
-    embed.setThumbnail(`https://image.tmdb.org/t/p/w500${filmData.poster_path}`);
-    embed.addField('Release Date', `${filmData.release_date}`);
-    embed.addField('Runtime', `${filmData.runtime} minutes`);
-    embed.addField('Rating', `${filmData.vote_average}  -  TheMovieDB User Score`);
-    embed.addField('Plot', filmData.overview);
-
-    message.channel.sendEmbed(embed);
+    return callback(searchData);
   });
-};
+}
 
 exports.conf = {
   enabled: true,
